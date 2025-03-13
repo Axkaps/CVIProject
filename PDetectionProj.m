af7101005f10fb64fe55779cc51ce7db845bd16c
Pedro Lima <pedro.f.c.lima@tecnico.ulisboa.pt>
2025-03-13 18:34:02 +0000

    added trajectory

--- PDetectionProj.m
+++ PDetectionProj.m
@@ -23,13 +23,14 @@ figure; hold on,
 imshow(bkg);
 
 % Lets define the strucutring element from now on
-nPedestrians = 10;
-pedestrianDb(nPedestrians) = struct("ID", 0, "Centroid", [0, 0]);
+% nPedestrians = 10;
+% pedestrianDb(nPedestrians) = struct("ID", 0, "Centroid", [0, 0], "BoundingBox", [0, 0, 0, 0]);
 
-for i = 1:10
-    pedestrianDb(i).ID = i;
-    pedestrianDb(i).Centroid = [i, i];
-end
+%for i = 1:10
+    %pedestrianDb(i).ID = i;
+    %pedestrianDb(i).Centroid = [i, i];
+    %pedestrianDb(i).BoundingBox = [i, i, i, i];
+%end
 
 % Now lets get the GroundTruth
 run("readGroundTruth.m");
